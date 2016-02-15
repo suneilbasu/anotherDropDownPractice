ac7b9b087769650c1002512b33a1b47dcd963e48
suneilbasu <suneil@futureprogress.com>
2016-02-15 17:35:01 +0000

    working filter using the controller rather than the model to filter search

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @students = Student.where(["tutor_id LIKE ?","%#{params[:student][:tutor_id]}%"])
   end
 
   def new
